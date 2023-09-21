71119e40da1592a8590942fa316275fad2b8a03d
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-09-21 10:07:17 +0300

    add FRotator variable in header file

--- ObstacleAssault/Source/ObstacleAssault/MovingPlatform.h
+++ ObstacleAssault/Source/ObstacleAssault/MovingPlatform.h
@@ -24,12 +24,14 @@ public:
 	virtual void Tick(float DeltaTime) override;
 
 private:
-	UPROPERTY(EditAnywhere, Category = "Moving Platform")
+	UPROPERTY(EditAnywhere, Category = "Moving")
 	FVector PlatformVelocity = FVector(100, 0, 0);
-	FVector StartLocation;
-	UPROPERTY(EditAnywhere, Category = "Moving Platform")
+	UPROPERTY(EditAnywhere, Category = "Moving")
 	float MoveDistance = 100;
+	UPROPERTY(EditAnywhere, Category = "Rotation")
+	FRotator RotationVelocity;
 
+			FVector StartLocation;
 	void MovePlatform(float DeltaTime);
 	void RotatePlatform(float DeltaTime);
 	bool ShouldPlatformReturn() const;
